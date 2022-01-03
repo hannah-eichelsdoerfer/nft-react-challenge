ce11d2cf9ff93120b7e7483ee7d61b3f1440e1f9
hannah-eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2022-01-03 02:16:20 +0100

    starter files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import axios from "axios";
+import NFTs from "./components/NFTs";
 
 function App() {
+  const [collectionData, setCollectionData] = useState([]);
+
+  const collection = "0x7a4c632eF997192C232551a2818952a07b58d415";
+
+  useEffect(() => {
+    const getNfts = async () => {
+      const openseaData = await axios.get(
+        `https://testnets-api.opensea.io/assets?asset_contract_address=${collection}&order_direction=asc`
+      );
+      console.log(openseaData.data.assets);
+      setCollectionData(openseaData.data.assets);
+    };
+
+    getNfts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <NFTs collectionData={collectionData} />
     </div>
   );
 }
